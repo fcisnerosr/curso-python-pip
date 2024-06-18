c0d43e17073e1f248a04ede49bc0ab1552bfe6ad
Gabinete <fcisneros@outlook.es>
2024-06-18 10:25:11 -0600

    integración de pandas en la app para hacer graficas, ademas de haber usado y entendido el uso de API en el directorio web-server

--- proyect-csv/app/main.py
+++ proyect-csv/app/main.py
@@ -1,27 +1,28 @@
 # Comentario agregado en GitHub
-
+import pandas as pd
 import utils
 import read_csv
 import charts
-import pandas as pd
+import os
+os.system('clear')
 
 def run():
-  '''
-  data = read_csv.read_csv('data.csv')
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  '''
-  df = pd.read_csv('data.csv')
-  df = df[df['Continent'] == 'Africa']
-  
-  countries = df['Country'].values
-  percentages = df['World Population Percentage'].values
-  charts.generate_pie_chart(countries, percentages)
+  # data = read_csv.read_csv('data.csv')
+  # data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
+  # countries = list(map(lambda x: x['Country'], data))
+  # percentages = list(map(lambda x: x['World Population Percentage'], data))
+  # charts.generate_pie_chart(countries, percentages)
+  
+  df          = pd.read_csv('data.csv')                   # lectura de csv con pandas as pd
+  df          = df[df['Continent'] ==  'Africa']   # Redefinición de dt, busca todos los paises de 'South America'
+  countries   = df['Country'].values                      # de la tabla dt, accede a los valores de la columna 'Country'
+  
+  percentages = df['World Population Percentage'].values
+  
   data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
-  print(country)
+  # print(country)
 
   result = utils.population_by_country(data, country)
 
@@ -33,3 +34,4 @@ def run():
 
 if __name__ == '__main__':
   run()
+  
